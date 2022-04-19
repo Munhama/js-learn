93fd407dd896eccb47c7cfd8c319a69808e956a3
Vlad Berejinsckij <johnytynky@gmail.com>
2022-04-19 16:34:54 +0700

    Sixteenth exercise

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -216,11 +216,53 @@ window.addEventListener('DOMContentLoaded', function () {
 
     dotsWrap.addEventListener('click', function (event) {
         for (let i = 0; i < dots.length + 1; i++) {
-            if (event.target.classList.contains('dot') && 
+            if (event.target.classList.contains('dot') &&
                 event.target == dots[i - 1]) {
                 currentSlide(i);
             }
         }
     });
+
+    // Calculator
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.textContent = 0;
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' || restDays.value == 0 || persons.value == 0) {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    restDays.addEventListener('change', function () {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (persons.value == '' || persons.value == 0 || restDays.value == 0) {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    place.addEventListener('change', function () {
+        if (persons.value == '' || restDays.value =='') {
+            totalValue.textContent = 0;
+        } else {
+            let a = total;
+            totalValue.textContent = a * this.options[this.selectedIndex].value;
+        }
+    })
 
 });
